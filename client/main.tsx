e88341da932d75a22270634b1172ade8f783cc38
Darrin Massena <darrin@massena.com>
2024-11-09 15:04:07 -0800

    Remove watermark

--- client/main.tsx
+++ client/main.tsx
@@ -3,8 +3,21 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+removeWatermark()
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
 		<App />
 	</React.StrictMode>
 )
+
+function removeWatermark() {
+	requestAnimationFrame(() => {
+		const watermark = document.getElementsByClassName('tl-watermark_SEE-LICENSE')
+		if (watermark.length > 0) {
+			watermark[0].remove()
+		} else {
+			requestAnimationFrame(removeWatermark)
+		}
+	})
+}
