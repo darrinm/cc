8dda5d6cd559b489f40ea40d185badd266cd04e1
Darrin Massena <darrin@massena.com>
2024-11-09 18:50:57 -0800

    WIP

--- client/main.tsx
+++ client/main.tsx
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+// Log the current URL and query params
+console.log(window.location.href)
+console.log(window.location.search)
+
+// Extract the pathname from the URL
+const pathname = window.location.pathname
+console.log(pathname)
+
 removeWatermark()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
