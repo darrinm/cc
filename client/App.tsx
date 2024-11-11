5234003c92dd4f36d8a913e289384c2be3129a43
Darrin Massena <darrin@massena.com>
2024-11-10 18:31:25 -0800

    DocumentPreview

--- client/App.tsx
+++ client/App.tsx
@@ -1,19 +1,12 @@
 import { useSync } from '@tldraw/sync';
-import {
-  atom,
-  DEFAULT_EMBED_DEFINITIONS,
-  SerializedStore,
-  TLComponents,
-  Tldraw,
-  TLRecord,
-  TLUiOverrides,
-} from 'tldraw';
+import { atom, DEFAULT_EMBED_DEFINITIONS, TLComponents, Tldraw, TLUiOverrides } from 'tldraw';
 import { getBookmarkPreview } from './getBookmarkPreview';
 import { multiplayerAssetStore } from './multiplayerAssetStore';
 import { LayerPanel } from './LayerPanel';
 import { embeds } from './Embeds';
 import { TextSearchPanel } from './TextSearchPanel';
 import './text-search.css';
+import { DocumentPreview } from './DocumentPreview';
 
 // Where is our worker located? Configure this in `vite.config.ts`
 const WORKER_URL = process.env.TLDRAW_WORKER_URL;
@@ -51,53 +44,36 @@ function App({ document, element }: { document: string | undefined; element: str
     // ...and how to handle static assets like images & videos
     assets: multiplayerAssetStore,
   });
+  console.log('store', store);
 
   if (!document) {
-    return <div>No document</div>;
+    return <div>Need a document</div>;
   }
 
-  // TODO: if element is not 'edit', set the initial state to the element
-  if (element !== 'edit') {
-    console.log(store);
-    if (store.status === 'synced-remote') {
-      const snapshot = store.store.getStoreSnapshot();
-      return <StorePreview store={snapshot.store} />;
-    } else {
-      return (
-        <div>
-          {store.status}: preview {element}
-        </div>
-      );
-    }
+  if (element === 'edit') {
+    return (
+      <div style={{ position: 'fixed', inset: 0 }}>
+        <Tldraw
+          embeds={[embeds, ...DEFAULT_EMBED_DEFINITIONS.filter((d) => d.type !== 'tldraw')]}
+          //deepLinks
+          maxAssetSize={100_000_000}
+          components={components}
+          overrides={overrides}
+          isShapeHidden={(s) => !!s.meta.hidden}
+          // we can pass the connected store into the Tldraw component which will handle
+          // loading states & enable multiplayer UX like cursors & a presence menu
+          store={store}
+          onMount={(editor) => {
+            console.log('Tldraw onMount');
+            // when the editor is ready, we need to register our bookmark unfurling service
+            editor.registerExternalAssetHandler('url', getBookmarkPreview);
+          }}
+        />
+      </div>
+    );
+  } else {
+    return <DocumentPreview store={store} />;
   }
-
-  return (
-    <div style={{ position: 'fixed', inset: 0 }}>
-      <Tldraw
-        embeds={[embeds, ...DEFAULT_EMBED_DEFINITIONS.filter((d) => d.type !== 'tldraw')]}
-        //deepLinks
-        maxAssetSize={100_000_000}
-        components={components}
-        overrides={overrides}
-        isShapeHidden={(s) => !!s.meta.hidden}
-        // we can pass the connected store into the Tldraw component which will handle
-        // loading states & enable multiplayer UX like cursors & a presence menu
-        store={store}
-        onMount={(editor) => {
-          // when the editor is ready, we need to register our bookmark unfurling service
-          editor.registerExternalAssetHandler('url', getBookmarkPreview);
-        }}
-      />
-    </div>
-  );
-}
-
-function StorePreview({ store }: { store: SerializedStore<TLRecord> }) {
-  return (
-    <div>
-      <pre>{JSON.stringify(store, null, 2)}</pre>
-    </div>
-  );
 }
 
 export default App;
