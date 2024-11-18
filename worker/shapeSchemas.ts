863faccb89e37f56576889cb9fde01e4438572f2
Darrin Massena <darrin@massena.com>
2024-11-18 08:24:47 -0800

    Add Shader

--- worker/shapeSchemas.ts
+++ worker/shapeSchemas.ts
@@ -69,7 +69,50 @@ const radixButtonShapeProps = {
   text: T.string,
 };
 
+export type GLSLType =
+  | 'float'
+  | 'int'
+  | 'bool'
+  | 'vec2'
+  | 'vec3'
+  | 'vec4'
+  | 'sampler2D'
+  | 'mat4'
+  | 'vec3[4]';
+
+export type UniformMetadata = {
+  [key: string]:
+    | string
+    | number
+    | boolean
+    | object
+    | Record<string, string | number | boolean | object>[];
+};
+
+export type UniformDescription = {
+  type: GLSLType;
+  name: string;
+  metadata: UniformMetadata;
+};
+
+const UDMetadata: T.ObjectValidator<UniformMetadata> = T.object({}).allowUnknownProperties();
+
+const UD: T.ObjectValidator<UniformDescription> = T.object({
+  name: T.string,
+  type: T.string as T.Validator<GLSLType>,
+  metadata: UDMetadata,
+});
+
+const shaderShapeProps = {
+  w: T.number,
+  h: T.number,
+  fragmentShader: T.string.optional(),
+  optimizeImages: T.boolean,
+  uniformDescriptions: T.arrayOf(UD),
+};
+
 export const customShapeSchemas = {
+  shader: { props: shaderShapeProps },
   button: { props: buttonShapeProps },
   'speech-bubble': { props: speechBubbleShapeProps },
   'radix-button': { props: radixButtonShapeProps },
